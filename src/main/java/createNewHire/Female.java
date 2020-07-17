91963aa13a4041def26af115443a01d73918e055
mekanhan <mekjanhan@gmail.com>
2020-07-17 00:55:34 -0500

    src/main/java/createNewHire/Female.java

--- src/main/java/createNewHire/Female.java
+++ src/main/java/createNewHire/Female.java
@@ -8,21 +8,24 @@ import java.util.Random;
 public class Female {
 
     // Drive Function
-    public static void main(String[] args)
-    {
-//        String firstNameFemale [] = {"Alliah", "Ella", "Rella", "Bella", "Valeria", "Lena",
-//                "Vika", "Victoria","Lisa", "Lada", "Sasha", "Alexia","Sarah", "Elizabeth" };
-        String lastName [] = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Moore",
-                "Thomas", "Taylor","Harris", "Martin", "Garcia", "Martinez","Lopez", "Hill",
-                "Allen", "Young", "Mitchell", "Perez", "Rogers" };
+    public static void main(String[] args) {
+
+        Female obj = new Female();
+
+        // salutation list
+        List<String> salutation = new ArrayList<>();
+        salutation.add("Ms.");
+        salutation.add("Mrs.");
+        salutation.add("Miss");
+        salutation.add("Ms.");
+        salutation.add("Dr.");
 
         // firstFemale list
         List<String> firstFemaleName = new ArrayList<>();
-
         firstFemaleName.add("Alliah");
         firstFemaleName.add("Ella");
         firstFemaleName.add("Rella");
-        firstFemaleName.add("Bella.");
+        firstFemaleName.add("Bella");
         firstFemaleName.add("Valeria");
         firstFemaleName.add("Lena");
         firstFemaleName.add("Vika");
@@ -34,17 +37,57 @@ public class Female {
         firstFemaleName.add("Sarah");
         firstFemaleName.add("Elizabeth");
 
+        // lastName list
+        List<String> lastName = new ArrayList<>();
+        lastName.add("Smith");
+        lastName.add("Johnson");
+        lastName.add("Williams");
+        lastName.add("Jones");
+        lastName.add("Brown");
+        lastName.add("Moore");
+        lastName.add("Thomas");
+        lastName.add("Taylor");
+        lastName.add("Harris");
+        lastName.add("Martin");
+        lastName.add("Garcia");
+        lastName.add("Martinez");
+        lastName.add("Lopez");
+        lastName.add("Hill");
+        lastName.add("Allen");
+        lastName.add("Young");
+        lastName.add("Mitchell");
+        lastName.add("Perez");
+        lastName.add("Rogers");
 
-        Female obj = new Female();
 
-        System.out.println(obj.getRandomFemaleName(firstFemaleName));
+                                    //Prints
+
+        System.out.println(obj.getRandomSalutation(salutation));
+        System.out.println(obj.getRandomLastName(firstFemaleName));
+        System.out.println(obj.getRandomFemaleName(lastName));
 
     }
+                                    //gets
 
-    // Function select an element base on index and return an element
-    public String getRandomFemaleName(List<String> list)
-    {
-        Random rand = new Random();
-        return list.get(rand.nextInt(list.size()));
-    }
+        // Function select an element base on index and return an element
+        public String getRandomFemaleName(List<String> list)
+        {
+        Random randFemName = new Random();
+        return list.get(randFemName.nextInt(list.size()));
+        }
+
+        // Function select an element base on index and return an element
+        public String getRandomLastName(List<String> list)
+        {
+        Random randLastName = new Random();
+        return list.get(randLastName.nextInt(list.size()));
+        }
+
+        // Function select an element base on index and return an element
+        public String getRandomSalutation(List<String> list)
+        {
+            Random randSalutation = new Random();
+            return list.get(randSalutation.nextInt(list.size()));
+        }
+
 }
